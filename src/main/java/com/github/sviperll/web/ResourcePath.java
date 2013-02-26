c9371f3b8e72242ec958999f7f9034b3dc78f745
Victor Nazarov <asviraspossible@gmail.com>
2013-02-26 14:06:16 +0400

    Refine ResourcePath methods.

--- src/main/java/com/github/sviperll/web/ResourcePath.java
+++ src/main/java/com/github/sviperll/web/ResourcePath.java
@@ -30,6 +30,14 @@ public class ResourcePath {
         return segments;
     }
 
+    public String segment(int index) {
+        return segments[index];
+    }
+
+    public String segmentWithoutPrefix(ResourcePath prefix, int index) {
+        return withoutPrefix(prefix).segment(index);
+    }
+
     /**
      * Each segment pattern can be either a string or null.
      * If segment pattern is a string it matches exact string in segments.
@@ -51,19 +59,11 @@ public class ResourcePath {
         return true;
     }
 
-    public boolean matches(ResourcePath prefix, String... segmentPatterns) {
-        int prefixLength = prefix.segments.length;
-        if (segments.length != prefixLength + segmentPatterns.length)
-            return false;
-        for (int i = 0; i < segments.length; i++) {
-            String segmentPattern = i < prefixLength ? prefix.segments[i] : segmentPatterns[i - prefixLength];
-            if (!(segmentPattern == null || segments[i].equals(segmentPattern)))
-                return false;
-        }
-        return true;
+    public boolean matchesWithoutPrefix(ResourcePath prefix, String... segmentPatterns) {
+        return withoutPrefix(prefix).matches(segmentPatterns);
     }
 
-    public boolean isEmpty() {
+    public boolean isRoot() {
         return segments.length == 0;
     }
 
@@ -95,8 +95,20 @@ public class ResourcePath {
             throw new IllegalArgumentException("Object is not ResourcePath: " + thatObject);
         else {
             ResourcePath that = (ResourcePath)thatObject;
-            return this.matches(that);
+            if (this.segments.length != that.segments.length)
+                return false;
+            for (int i = 0; i < this.segments().length; i++)
+                if (!this.segments[i].equals(that.segments[i]))
+                    return false;
+            return true;
         }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Arrays.deepHashCode(this.segments);
+        return hash;
     }
 
     public boolean startsWith(ResourcePath prefix) {
@@ -108,10 +120,6 @@ public class ResourcePath {
         return true;
     }
 
-    public boolean startsWith(String... segments) {
-        return startsWith(new ResourcePath(segments));
-    }
-
     public ResourcePath withoutPrefix(ResourcePath prefix) {
         if (this.segments.length < prefix.segments.length)
             throw new IllegalArgumentException("Incorect prefix: " + prefix);
@@ -120,9 +128,5 @@ public class ResourcePath {
                 throw new IllegalArgumentException("Incorect prefix: " + prefix);
         String[] result = Arrays.copyOfRange(segments, prefix.segments.length, segments.length);
         return new ResourcePath(result);
-    }
-
-    public ResourcePath withoutPrefix(String... segments) {
-        return withoutPrefix(new ResourcePath(segments));
     }
 }
